cb3cfaa44e1d68ccb81309e7287262f1a0abddb6
Moshiur Rahman <unimrgm@gmail.com>
2021-08-15 01:03:30 +0600

    removed clonflicting css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div
-      class="px-4 py-6 mt-10  sm:px-0 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+      class="px-4 py-6 mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
     >
       <div class="sm:text-center lg:text-left">
         <h2
