501de9b50829db3bdd3a7f5ad53c09628ea2a07d
Moshiur Rahman <unimrgm@gmail.com>
2021-08-15 00:59:16 +0600

    removed links

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,7 +21,7 @@
         >
           All the code present here is part of a free sample from
           <a
-            href="https://tailwindui.com"
+            href="#"
             class="text-indigo-600 underline hover:text-indigo-500"
             >Tailwind UI</a
           >
@@ -29,14 +29,14 @@
           <b>Tailwind UI </b>&#32; check the instructions on
           <a
             class="text-indigo-600 underline hover:text-indigo-500"
-            href="https://github.com/posva/vite-tailwind-starter/tree/master#vite--tailwind-css-starter"
+            href="#"
             ><code>README</code></a
           >. It only takes two steps to add it 🙂.
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div class="rounded-md shadow">
             <a
-              href="https://github.com/posva/vite-tailwind-starter"
+              href="#"
               class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md  hover:bg-indigo-500 focus:outline-none focus:ring md:py-4 md:text-lg md:px-10"
             >
               Get started
